15e73965b57b10ea1460b4307bac30f90f4ef45a
güney <guneyguven@users.noreply.github.com>
2026-08-12 00:35:50 +0300

    Configure GitHub Pages project base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://guneyguven2.github.io',
+  site: 'https://guneyguven.github.io',
+  base: '/guneyguven2.github.io',
   integrations: [react(), mdx()],
   output: 'static',
   build: { format: 'directory' }
