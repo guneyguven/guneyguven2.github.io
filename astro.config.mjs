6f08c07ded2198293b0506b95a37ce5d2e50b93f
güney <guneyguven@users.noreply.github.com>
2026-08-12 00:37:28 +0300

    Set canonical GitHub Pages project URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://guneyguven.github.io',
+  site: 'https://guneyguven.github.io/guneyguven2.github.io',
   base: '/guneyguven2.github.io',
   integrations: [react(), mdx()],
   output: 'static',
